ba354ef519214cc7799d9174fe1f51eefea7d723
Vilson Vieira <vilson@void.cc>
2014-11-25 12:09:19 -0200

    Add better CLI

--- runSaliency.cpp
+++ runSaliency.cpp
@@ -5,6 +5,10 @@ in CVPR13.
 written by Chuan Yang
 email: ycscience86@gmail.com
 date: 2013.5.7
+
+modified by Vilson Vieira
+email: vilson@void.cc
+date: 2014.11.28
 */
 
 #include <cstring>
@@ -12,15 +16,16 @@ date: 2013.5.7
 
 #define MAX_PATH 255
 
-int main(int argc,char *argv[]) {
-	//char imname[MAX_PATH+1];
+int main(int argc, char *argv[]) {
 	Mat sal, img;
-  char imname[] = "test_set/_MG_0070.JPG";
-  img=imread(imname);
+	char imname[MAX_PATH+1];
+	strcpy(imname, argv[1]);
+
+  img = imread(imname);
 	GMRsaliency GMRsal;
-	sal=GMRsal.GetSal(img);
+	sal = GMRsal.GetSal(img);
 	char salname[MAX_PATH+1];
-	sprintf(salname,"%s_saliency.png",imname);
+	sprintf(salname, "%s_saliency.png", imname);
 	imwrite(salname,sal*255);
   
 	return 0;
